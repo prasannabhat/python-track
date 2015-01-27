b4edf791e91f9af1c51b54851e342b58ebb495ac
Prasanna Bhat <prasanna.bhat>
2015-01-27 23:12:51 +0530

    Introduce attribute for array

--- track.py
+++ track.py
@@ -1,4 +1,5 @@
 from library import argparser as parse
+args = {}
 
 try:
 	args = parse.parse_args()
@@ -6,5 +7,7 @@ except BaseException as e:
 	pass
 	# print(e)
 else:
+	pass
+finally:
 	print (args)
 
