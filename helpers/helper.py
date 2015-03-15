c7a9a37f1eb282a1b01ed1c43908d75f8e3cb1e0
Prasanna Bhat <prasanna.bhat>
2015-03-16 00:46:36 +0530

    arguments with - characters are creating problem.Hence use sync_key instead of sync-key
    Suppress the exception about the missing arguments. Combine the arguments from xml with command line args and parse at the end

--- helpers/helper.py
+++ helpers/helper.py
@@ -15,7 +15,7 @@ def get_parser():
 	parser.add_argument("-u", "--user", help="User name , required to login to CSCRM",required=True)
 	parser.add_argument("-p", "--password" , help="Password for CSCRM", required=True)
 	parser.add_argument("-q", "--query" , help="Query which should be run to fetch the data from", required=True)
-	parser.add_argument("-sk", "--sync-key" , help="Primary key for syncing. If there are multiple keys seperate them using :", required=True)
+	parser.add_argument("-sk", "--sync_key" , help="Primary key for syncing. If there are multiple keys seperate them using :", required=True)
 
 	# Optional arguments , which are optional
 	parser.add_argument("-x", "--xml", help=""" XML file which contains the options. Even the Mandatory parameters can be specified in XML file
--- library/argparser.py
+++ library/argparser.py
@@ -4,8 +4,21 @@ from xml.etree.ElementTree import parse as elemTree
 
 class MyArgumentParser(argparse.ArgumentParser):
 	"""Extend the ArgumentParser"""
+	
 	def __init__(self, parents=[]):
 		super(MyArgumentParser, self).__init__(parents=parents,add_help=False)
+
+	def error(self, message):
+	    """error(message: string)
+	    IMP : Very tricky override to solve the problem
+	    If the error message says "the following arguments are required" , then skip this error and continue
+	    This way atleast we get the parsed object which will not contains some of the required arguments
+	    For any other type of errors, report error
+	    """
+	    text = "the following arguments are required"
+	    if text in message:
+	    	return
+	    super(MyArgumentParser,self).error(message)
 
 
 def parse_args(parser, xml_file = None , args = None):
@@ -21,19 +34,22 @@ def parse_args(parser, xml_file = None , args = None):
 	# If user has provided args as parameter use it, else use the command line arguments
 	# The first command line argument would be the python file name, skip it
 	args = args if args else sys.argv[1:]
-	# Parse using custom parser
+	# Parse using custom parser. Custom parser would still return namespace even if some of the parameters are present.
 	argsNamespace = p.parse_args(args)
 	argsDictionary = argsNamespace.__dict__
+	
 	# If XML file is given which also can supply arguments, process the same
 	if (xml_file != None and argsDictionary[xml_file] != None):
 		# Read additional arguments from the XML file
 		additionalArgs = __read_additional_args__(argsDictionary, argsDictionary[xml_file])
 		# Add the additional arguments from xml file to args
 		args.extend(additionalArgs)
-		# Parse again and return the new one
-		argsNamespace = p.parse_args(args)
-		return argsNamespace
+
+	# Parse again and return the new one, but this time parse using the original parser.
+	# Any errors about missing required arguments should be reported now
+	argsNamespace = parser.parse_args(args)
 	return argsNamespace
+
 
 def __read_additional_args__(dict, xml_file):
 	args = []
